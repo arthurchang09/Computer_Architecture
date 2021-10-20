2356377d274f800766dac89d3b46cecd7a44c200
arthurchang09 <58202972+arthurchang09@users.noreply.github.com>
2021-10-20 20:47:59 +0800

    Add new comments
    Put more comments in the code so that people can know how the code run.

--- Defuse_the_Bomb.s
+++ Defuse_the_Bomb.s
@@ -1,5 +1,5 @@
 .data
-arr1:     .word 5,7,1,4 # a[4] = {4,5,8,12}
+arr1:     .word 5,7,1,4 # a[4] = {5,7,1,4}
 len:      .word 16 # 4*4
 target:   .word 3
 comma:    .string " "
@@ -10,8 +10,8 @@ main:
     la a0 arr1    # load address of the array
     lw a1 target  # load the value of target
     lw a2 len     # load the value of len
-    add a2,a2,a0  # turn a2 into the end of array 
-    addi a3,a2 -4 # a3 is the address of the last element
+    add a2,a2,a0  # turn a2 into the the address of end of array 
+    addi a3,a2 -4 # a3 is the address of the last element in array
     jal ra, func
     
     # Exit Program
@@ -21,24 +21,24 @@ main:
     
 func:
     mv s9 a0 
-    beq a1,x0 Zero
-    blt a1,x0 small
+    beq a1,x0 Zero  # if k == 0, goto Zero
+    blt a1,x0 small # if k < 0, goto small
 large:
-    sub t0, a2,a0    # t0 is the number of times thar the loop must run
+    sub t0, a2,a0    # t0 is the number of times that the loop must run
     addi t1,a0 0        # set t1 the address of a[0]
 loop_large:
     addi t0,t0 -4
     mv s0,a1    # s0 = target, which count the times that the inner loop has run
-    mv s1,t1
+    mv s1,t1    # s1 = the address of the number needs to be added
     addi s2,x0 0
 loop_in_large:
     addi s0,s0 -1
-    addi s1,s1 4        # address of a[j]
-    blt s1 a2 less_than_end
+    addi s1,s1 4        # s1 = address of the next element
+    blt s1 a2 less_than_end # if s1 is smaller than address of the end array a
     add s1,x0,s9
 less_than_end:
-    lw s3,(0)s1    # load a[j]
-    add s2,s2,s3    # add a[j]
+    lw s3,(0)s1    # load value in s1
+    add s2,s2,s3    # add value in s1
     
     bne s0,x0 loop_in_large
     #print result
@@ -56,22 +56,22 @@ less_than_end:
     
     
 small:
-    sub t0, a2, a0    # t0 is the number of times thar the loop must run
+    sub t0, a2, a0    # t0 is the number of times that the loop must run
     addi t1,a0 0        # set t1 the address of a[0]
 loopsmall:
     addi t0,t0 -4
     mv s0,a1    # s0 = target, which count the times that the inner loop has run
-    mv s1,t1
+    mv s1,t1    # s1 = the address of the number needs to be added
     addi s2,x0 0
 loop_in_small:
     addi s0,s0,1 # 
-    addi s1,s1,-4    # address of a[j]
-    bge s1,s9 gre_than_arr # if address of a[j] is larger than address of a
-    add s1,x0,a3    # if address of a[j] is larger than address of a, 
+    addi s1,s1,-4     # s1 = address of the previous element
+    bge s1,s9 gre_than_arr # if s1 is larger than address of array a
+    add s1,x0,a3    # if s1 is smaller than address of a, 
                     # move it to the last element of the array. 
 gre_than_arr:
-    lw s3,(0)s1    # load a[j]
-    add s2,s2,s3    # add a[j]
+    lw s3,(0)s1    # load value in s1
+    add s2,s2,s3    # add value in s1
     
     bne s0,x0 loop_in_small
     #print result
@@ -89,6 +89,7 @@ gre_than_arr:
     
 Zero:
     sub t0, a2,a0
+    # Print 0s
 loop0:
     
     mv a0,x0
